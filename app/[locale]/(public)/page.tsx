79bc9215d25c3670c577bd9bb23587e72219602a
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2025-09-24 17:42:48 +0000

    Correction des composants recu de la branche LUNION-237

--- app/[locale]/(public)/page.tsx
+++ app/[locale]/(public)/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
 			/>
 			<ActualitesVaticanSection/>
 			<EvenementList/>
-			<InvolvementSection/>
+			<InvolvementSection />
 			<MissionSignup/>
 		</Content>
 	);
