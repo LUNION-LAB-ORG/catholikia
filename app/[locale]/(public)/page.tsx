a0e9d6b485047e7f6e172a226a4d864df9cb015d
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2025-09-24 14:28:28 +0000

    US-LUNION-234: Faire défiler les informations(CATHOLIKIA)

--- app/[locale]/(public)/page.tsx
+++ app/[locale]/(public)/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/(public)/accueil/hero-section";
 import ActualitesRecentesSection from "@/components/(public)/accueil/actualites-recentes-section";
 import Publicite from "@/components/(public)/publicites";
 import ActualitesVaticanSection from "@/components/(public)/accueil/actualites-vatican-section";
+import FlashInfo from "@/components/(public)/accueil/flash-infos";
 
 export default async function Home() {
 
@@ -11,7 +12,7 @@ export default async function Home() {
 			<HeroSection/>
 			<Content fullWidth>
 				<ActualitesRecentesSection/>
-				{/*<FlashInfo/>*/}
+				<FlashInfo/>
 				<Publicite
 					position="ACCUEIL_MIDDLE"
 					orientation="horizontal"
