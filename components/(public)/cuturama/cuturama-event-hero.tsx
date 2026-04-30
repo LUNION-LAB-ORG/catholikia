14cd625efa0cf7adb51d01dcaa9bccd5544f4179
niortien <niortiencoulibaly2001@gmail.com>
2026-04-30 10:45:49 +0000

    Add missing return statement for ad spot link rendering in EventHero component

--- components/(public)/cuturama/cuturama-event-hero.tsx
+++ components/(public)/cuturama/cuturama-event-hero.tsx
@@ -79,6 +79,7 @@ export function EventHero({ event }: EventHeroProps) {
                             </>
                         );
 
+                        
                         return spot.linkUrl ? (
                             <Link
                                 href={spot.linkUrl}
